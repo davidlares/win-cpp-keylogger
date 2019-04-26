f75e0e174c951beb7959e590d86be10232b408e0
= <=>
2019-04-26 00:24:05 -0400

    Hiding console prompt - compiler settings for IDE

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	cout << "Hello World" << endl;
+	
+	// disabling render prompt
+	MSG Msg;
+	// infinite loop for taking messages
+	while(GetMessage (&Msg, NULL, 0, 0)) {
+		// takes a message intended for a GUI
+		TranslateMessage(&Msg);
+		// forward the message to the system
+		DispatchMessage(&Msg);
+	}
 	return 0;
 }
